c158428c19f8264ef8872a597ba008bc47c708fe
Dan Fisher <dan.fisher85@yahoo.com>
2025-10-08 19:37:31 +0200

    fixes issue with widget customization options

--- includes/class-mtp-table-widget.php
+++ includes/class-mtp-table-widget.php
@@ -36,6 +36,12 @@ class MTP_Table_Widget extends WP_Widget {
       // Prepare shortcode attributes (width and height are now auto-determined)
       $shortcode_atts = array('post_id' => $table_id);
 
+      // Load all styling parameters from post meta to ensure customizations are applied
+      $shortcode_atts = array_merge($shortcode_atts, $this->get_styling_attributes_from_meta($table_id));
+
+      // Load other configuration parameters from post meta
+      $shortcode_atts = array_merge($shortcode_atts, $this->get_config_attributes_from_meta($table_id));
+
       // Use the existing shortcode functionality
       $mtp_plugin = MTP_Plugin::instance();
       $shortcode = new MTP_Table_Shortcode($mtp_plugin->table_renderer);
@@ -95,5 +101,119 @@ class MTP_Table_Widget extends WP_Widget {
     $instance['table_id'] = (!empty($new_instance['table_id'])) ? absint($new_instance['table_id']) : '';
 
     return $instance;
+  }
+
+  /**
+   * Get styling attributes from post meta
+   */
+  private function get_styling_attributes_from_meta($table_id) {
+    $attributes = array();
+
+    // Define parameter mapping from meta keys to shortcode attribute names
+    $param_mapping = array(
+      '_mtp_font_size' => 's-size',
+      '_mtp_header_font_size' => 's-sizeheader',
+      '_mtp_text_color' => 's-color',
+      '_mtp_main_color' => 's-maincolor',
+      '_mtp_table_padding' => 's-padding',
+      '_mtp_inner_padding' => 's-innerpadding',
+      '_mtp_logo_size' => 's-logosize',
+      '_mtp_border_color' => 's-bcolor',
+      '_mtp_bsizeh' => 's-bsizeh',
+      '_mtp_bsizev' => 's-bsizev',
+      '_mtp_bsizeoh' => 's-bsizeoh',
+      '_mtp_bsizeov' => 's-bsizeov',
+      '_mtp_head_bottom_border_color' => 's-bbcolor',
+      '_mtp_bbsize' => 's-bbsize',
+    );
+
+    // Get simple color/styling values
+    foreach ($param_mapping as $meta_key => $attr_name) {
+      $value = get_post_meta($table_id, $meta_key, true);
+      if (!empty($value)) {
+        $attributes[$attr_name] = $value;
+      }
+    }
+
+    // Handle color+opacity combinations
+    $color_opacity_mapping = array(
+      '_mtp_bg_color' => array('attr' => 's-bgcolor', 'opacity_meta' => '_mtp_bg_opacity'),
+      '_mtp_even_bg_color' => array('attr' => 's-bgeven', 'opacity_meta' => '_mtp_even_bg_opacity'),
+      '_mtp_odd_bg_color' => array('attr' => 's-bgodd', 'opacity_meta' => '_mtp_odd_bg_opacity'),
+      '_mtp_hover_bg_color' => array('attr' => 's-bgover', 'opacity_meta' => '_mtp_hover_bg_opacity'),
+      '_mtp_head_bg_color' => array('attr' => 's-bghead', 'opacity_meta' => '_mtp_head_bg_opacity'),
+    );
+
+    foreach ($color_opacity_mapping as $color_meta => $config) {
+      $color = get_post_meta($table_id, $color_meta, true);
+      $opacity = get_post_meta($table_id, $config['opacity_meta'], true);
+
+      if (!empty($color)) {
+        $combined_color = $this->combine_color_opacity($color, $opacity);
+        $attributes[$config['attr']] = $combined_color;
+      }
+    }
+
+    return $attributes;
+  }
+
+  /**
+   * Get configuration attributes from post meta
+   */
+  private function get_config_attributes_from_meta($table_id) {
+    $attributes = array();
+
+    // Define boolean parameter mapping
+    $boolean_params = array(
+      '_mtp_suppress_wins' => 'sw',
+      '_mtp_suppress_logos' => 'sl',
+      '_mtp_suppress_num_matches' => 'sn',
+      '_mtp_projector_presentation' => 'bm',
+      '_mtp_navigation_for_groups' => 'nav',
+    );
+
+    foreach ($boolean_params as $meta_key => $attr_name) {
+      $value = get_post_meta($table_id, $meta_key, true);
+      if ($value === '1') {
+        $attributes[$attr_name] = '1';
+      }
+    }
+
+    // Get language setting
+    $language = get_post_meta($table_id, '_mtp_language', true);
+    if (!empty($language)) {
+      $attributes['lang'] = $language;
+    }
+
+    // Get group setting
+    $group = get_post_meta($table_id, '_mtp_group', true);
+    if (!empty($group)) {
+      $attributes['group'] = $group;
+    }
+
+    return $attributes;
+  }
+
+  /**
+   * Combine hex color and opacity percentage into 8-character hex
+   */
+  private function combine_color_opacity($hex_color, $opacity_percent) {
+    // Remove # if present
+    $hex_color = ltrim($hex_color, '#');
+
+    // If color already has alpha (8 characters), return as is
+    if (strlen($hex_color) == 8) {
+      return $hex_color;
+    }
+
+    // If no opacity specified, default based on context
+    if ($opacity_percent === '' || $opacity_percent === null) {
+      $opacity_percent = 100; // Default to fully opaque
+    }
+
+    // Convert opacity percentage to hex
+    $opacity_hex = str_pad(dechex(round(($opacity_percent / 100) * 255)), 2, '0', STR_PAD_LEFT);
+
+    return $hex_color . $opacity_hex;
   }
 }
